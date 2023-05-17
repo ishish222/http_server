0b98eb5a07b6f7a1ec4f8c70702e5a30851d3d18
korrino <tomasz.salacinski@korrino.com>
2023-05-17 20:45:50 +0200

    About to implement request parsing

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,6 @@
 use std::net::TcpListener;
+use std::io::Read;
+use crate::http::Request;
 
 pub struct Server {
     addr: String,
@@ -13,5 +15,29 @@ impl Server {
         println!("Listening on {}", self.addr);
 
         let listener = TcpListener::bind(&self.addr).unwrap();
+
+        loop {
+            let res = listener.accept();
+            match res {
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer)
+                    {
+                        Ok(_) => {
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+                            match Request::try_from(&buffer[..])
+                            {
+                                Ok(request) => {
+                                    
+                                },
+                                Err(e) => println!("Failed to parse: {}", e),
+                            }
+                        },
+                        Err(e) => println!("Failed to read from connection: {}", e),
+                    }
+                },
+                Err(e) => println!("Failed to establish connection: {}", e),
+            }
+        }
     }
 }
